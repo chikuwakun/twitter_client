6d678bec55abf32a78872de4e0f75dacaa490a7a
chikuwakun <tamamo0312@gmail.com>
2022-07-08 14:56:33 +0900

    add twitterAouth1
    we can use twitterAPi and get tokens

--- lib/widgets/twitterapp.dart
+++ lib/widgets/twitterapp.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:oauth1/oauth1.dart' as oauth1;
+import '../credentials.dart' as credentials;
+import 'package:url_launcher/url_launcher.dart';
+
 
 class TwitterApp extends StatefulWidget {
   const TwitterApp({Key? key}) : super(key: key);
@@ -8,6 +12,30 @@ class TwitterApp extends StatefulWidget {
 }
 
 class _TwitterAppState extends State<TwitterApp> {
+  final controller = TextEditingController();
+  final platform =oauth1.Platform(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/authorize',
+    'https://api.twitter.com/oauth/access_token',
+    oauth1.SignatureMethods.hmacSha1,
+  );
+  final clientCredentials = oauth1.ClientCredentials(
+    credentials.apikey,
+    credentials.keysecret,
+  );
+
+  late final auth = oauth1.Authorization(clientCredentials,platform);
+  oauth1.Credentials? tokenCredentials;
+
+  @override
+  void initState() {
+    super.initState();
+
+    auth.requestTemporaryCredentials('oob').then((res){
+      tokenCredentials = res.credentials;
+      launch(auth.getResourceOwnerAuthorizationURI(tokenCredentials!.token));//TODO ここ新しい関数にする
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +43,37 @@ class _TwitterAppState extends State<TwitterApp> {
         title: Text('Twitter'),
       ),
       body: Center(
-        child: Text('Twitter'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            TextFormField(
+              controller:controller
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                // 入力されたPINを元に Access Token を取得
+                final pin = controller.text;
+                final verifier = pin;
+                final res = await auth.requestTokenCredentials(
+                  tokenCredentials!,
+                  verifier,
+                );
+                // 取得した Access Token を使ってAPIにリクエストできる
+                final client = oauth1.Client(
+                  platform.signatureMethod,
+                  clientCredentials,
+                  res.credentials,
+                );
+                final apiResponse = await client.get(
+                  Uri.parse(
+                      'https://api.twitter.com/1.1/statuses/home_timeline.json?count=1'),
+                );
+                print(apiResponse.body);
+              },
+              child: Text('OK'),
+            ),
+          ]
+        ),
       ),
     );
   }
